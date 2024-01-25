077e9c556c7e52a83922e64a15ccd8323e22187b
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2024-01-25 16:34:52 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 python --version
-pip install scikit-learn==1.2.1
-pip install pandas==2.0.3
-pip install imbalanced-learn==0.11.0
-pip install xgboost==2.0.0
-pip install lightgbm==4.1.0
-pip install catboost==1.2.2
-pip install yellowbrick==1.5
+pip install scikit-learn==1.2.1 --force-reinstall
+pip install pandas==2.0.3 --force-reinstall
+pip install imbalanced-learn==0.11.0 --force-reinstall
+pip install xgboost==2.0.0 --force-reinstall
+pip install lightgbm==4.1.0 --force-reinstall
+pip install catboost==1.2.2 --force-reinstall
+pip install yellowbrick==1.5 --force-reinstall
 conda install python-graphviz
 conda --version
