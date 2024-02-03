e1ff59a6485ffc72351260d44550084da4c04131
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2024-02-03 13:32:40 +0800

    Update install.bat
    Fixed the sh*t show of compatibility issues between packages

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 python --version
-pip install scikit-learn==1.2.1 --force-reinstall
-pip install pandas==2.0.3 --force-reinstall
-pip install imbalanced-learn==0.11.0 --force-reinstall
 ::pip install xgboost==2.0.0 --force-reinstall
 pip install lightgbm==4.1.0 --force-reinstall
 pip install catboost==1.2.2 --force-reinstall
+pip install scikit-learn==1.2.1 --force-reinstall
+pip install imbalanced-learn==0.11.0 --force-reinstall
+pip install pandas==2.0.3 --force-reinstall
 pip install yellowbrick==1.5 
 pip install statsmodels==0.14.1 
 pip install mlxtend 
